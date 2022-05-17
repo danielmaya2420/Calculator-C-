e252ee541af50b20da1a91a08bbc19df7a79968e
DANIEL <DANIEL@DESKTOP-CJRDVHJ>
2022-05-17 12:12:02 -0500

    Agregar archivos de proyecto.

---
+++ Calculator/Form1.cs
@@ -0,0 +1,291 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public enum Operacion
+    {
+        NoDefinida = 0,
+        Mas = 1,
+        Resta = 2,
+        Division = 3,
+        Multiplicar = 4,
+        Modulo = 5
+    }
+    public partial class Form1 : Form
+    {
+        double valor1 = 0;
+        double valor2 = 0;
+        Operacion operador = Operacion.NoDefinida;
+        bool unNumeroLeido = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private double EjecutarOperacion()
+        {
+            double resultado = 0;
+            switch (operador)
+            {
+                case Operacion.Mas:
+                    resultado = valor1 + valor2;
+                    break;
+                case Operacion.Resta:
+                    resultado = valor1 - valor2;
+                    break;
+                case Operacion.Division:
+                    if (valor2 == 0)
+                    {
+                        MessageBox.Show("No se puede dividir entre 0");
+                        resultado = 0;
+                    }
+                    else
+                    {
+                        resultado = valor1 / valor2;
+                    }
+                    break;
+                case Operacion.Multiplicar:
+                    resultado = valor1 * valor2;
+                    break;
+                case Operacion.Modulo:
+                    resultado = valor1 % valor2;
+                    break;
+            }
+
+            return resultado;
+        }
+
+        private void btnCero_Click(object sender, EventArgs e)
+        {
+            unNumeroLeido = true;
+            if (cajaResultado.Text == "0")
+            {
+                return;
+            }
+            else
+            {
+                cajaResultado.Text += "0";
+            }
+
+        }
+
+        private void btnUno_Click(object sender, EventArgs e)
+        {
+            unNumeroLeido = true;
+            if (cajaResultado.Text == "0" && cajaResultado.Text != null)
+            {
+                cajaResultado.Text = "1";
+            }
+            else
+            {
+                cajaResultado.Text += "1";
+            }
+
+        }
+
+        private void btnDos_Click(object sender, EventArgs e)
+        {
+            unNumeroLeido = true;
+            if (cajaResultado.Text == "0" && cajaResultado.Text != null)
+            {
+                cajaResultado.Text = "2";
+            }
+            else
+            {
+                cajaResultado.Text += "2";
+            }
+        }
+
+        private void btnTres_Click(object sender, EventArgs e)
+        {
+            unNumeroLeido = true;
+            if (cajaResultado.Text == "0" && cajaResultado.Text != null)
+            {
+                cajaResultado.Text = "3";
+            }
+            else
+            {
+                cajaResultado.Text += "3";
+            }
+        }
+
+        private void btnCuatro_Click(object sender, EventArgs e)
+        {
+            unNumeroLeido = true;
+            if (cajaResultado.Text == "0" && cajaResultado.Text != null)
+            {
+                cajaResultado.Text = "4";
+            }
+            else
+            {
+                cajaResultado.Text += "4";
+            }
+        }
+
+        private void btnCinco_Click(object sender, EventArgs e)
+        {
+            unNumeroLeido = true;
+            if (cajaResultado.Text == "0" && cajaResultado.Text != null)
+            {
+                cajaResultado.Text = "5";
+            }
+            else
+            {
+                cajaResultado.Text += "5";
+            }
+        }
+
+        private void btnSeis_Click(object sender, EventArgs e)
+        {
+            unNumeroLeido = true;
+            if (cajaResultado.Text == "0" && cajaResultado.Text != null)
+            {
+                cajaResultado.Text = "6";
+            }
+            else
+            {
+                cajaResultado.Text += "6";
+            }
+        }
+
+        private void btnSiete_Click(object sender, EventArgs e)
+        {
+            unNumeroLeido = true;
+            if (cajaResultado.Text == "0" && cajaResultado.Text != null)
+            {
+                cajaResultado.Text = "7";
+            }
+            else
+            {
+                cajaResultado.Text += "7";
+            }
+        }
+
+        private void btnOcho_Click(object sender, EventArgs e)
+        {
+            unNumeroLeido = true;
+            if (cajaResultado.Text == "0" && cajaResultado.Text != null)
+            {
+                cajaResultado.Text = "8";
+            }
+            else
+            {
+                cajaResultado.Text += "8";
+            }
+        }
+
+        private void btnNueve_Click(object sender, EventArgs e)
+        {
+            unNumeroLeido = true;
+            if (cajaResultado.Text == "0" && cajaResultado.Text != null)
+            {
+                cajaResultado.Text = "9";
+            }
+            else
+            {
+                cajaResultado.Text += "9";
+            }
+        }
+        private void ObtenerValor(string operador)
+        {
+            valor1 = Convert.ToDouble(cajaResultado.Text);
+            lblHistorial.Text = cajaResultado.Text + operador;
+            cajaResultado.Text = "0";
+        }
+        private void btnMas_Click(object sender, EventArgs e)
+        {
+            operador = Operacion.Mas;
+            ObtenerValor("+");  
+        }
+
+        private void btnResultado_Click(object sender, EventArgs e)
+        {
+            if (valor2 == 0 && unNumeroLeido)
+            {
+                valor2 = Convert.ToDouble(cajaResultado.Text);
+                lblHistorial.Text += valor2 + "=";
+                double resultado = EjecutarOperacion();
+                valor1 = 0;
+                valor2 = 0;
+                unNumeroLeido = false;
+                cajaResultado.Text = Convert.ToString(resultado);
+            }
+        }
+
+        private void btnResta_Click(object sender, EventArgs e)
+        {
+            operador = Operacion.Resta;
+            ObtenerValor("-");
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            operador = Operacion.Multiplicar;
+            ObtenerValor("x");
+        }
+
+        private void btnDivision_Click(object sender, EventArgs e)
+        {
+            operador = Operacion.Division;
+            ObtenerValor("/");
+        }
+
+        private void btnModulo_Click(object sender, EventArgs e)
+        {
+            operador = Operacion.Modulo;
+            ObtenerValor("%");
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            cajaResultado.Text = "0";
+            lblHistorial.Text = "";
+        }
+
+        private void btnBorrar_Click(object sender, EventArgs e)
+        {
+            if(cajaResultado.Text.Length > 1)
+            {
+                string txtResultado = cajaResultado.Text;
+                txtResultado = txtResultado.Substring(0, txtResultado.Length - 1);
+
+                if (txtResultado.Length == 1 && txtResultado.Contains("-"))
+                {
+                    cajaResultado.Text = "0";
+                }
+                else
+                {
+                    cajaResultado.Text = txtResultado;
+                }
+
+  
+            }
+            else
+            {
+                cajaResultado.Text = "0";
+            }
+        }
+
+        private void btnPunto_Click(object sender, EventArgs e)
+        {
+            if (cajaResultado.Text.Contains("."))
+            {
+                return;
+            }
+            cajaResultado.Text += ".";
+        }
+    }
+}
